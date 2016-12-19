42938bf9a7a88600265e2264029f14ec4b9ddbb2
Ben Frisby <bfrisb@gmail.com>
2016-12-19 14:53:30 -0800

    fixed toggle views between slider and image

--- js/main.js
+++ js/main.js
@@ -2,36 +2,44 @@ $(function() {
 
 
 // plus sign button and bx-slider show and hide.
-  $('.mainButton').on("click", (e) => {
+$('.mainButton').on("click", (e) => {
 
   e.preventDefault();
   $('#sliderContainer').toggleClass('hide');
   $('#mainBody').toggleClass('hide');
   $('.mainButton').toggleClass('hide');
-  
+  $('.sliderButton').removeClass('hide');
+
   $('.bxslider').bxSlider();
-  
 
-  })
+})
+// slider view toggleClass
+$('.sliderButton').on("click", (e)=> {
+  $('#sliderContainer').toggleClass('hide');
+  $('#mainBody').toggleClass('hide');
+  $('.mainButton').toggleClass('hide');
+  $('.sliderButton').addClass('hide');
 
 
+})
 
-  function fullScreenSize() {
 
-    $('.fullDisplay').css({
-      top: '0',
-      position: 'absolute',
-      width: $(window).width(),
-      height: $(window).height()
-    });
+function fullScreenSize() {
 
-  }
-
-  $(window).resize(function() {
-    fullScreenSize();         
+  $('.fullDisplay').css({
+    top: '0',
+    position: 'absolute',
+    width: $(window).width(),
+    height: $(window).height()
   });
 
-  fullScreenSize();
+}
+
+$(window).resize(function() {
+  fullScreenSize();         
+});
+
+fullScreenSize();
 
 
 
